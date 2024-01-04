7b388ef2ba244f64da0e9e37fb4ede25ba954009
glaucoSapucaia <glaucopco@gmail.com>
2024-01-04 14:54:08 -0300

    State

---
+++ behavioral/state/state_1.py
@@ -0,0 +1,97 @@
+'''The state pattern exclude several conditionals operations | if ...'''
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Order:
+    '''Context Class'''
+
+    def __init__(self) -> None:
+        self.state: OrderState = PaymentPending(self)
+
+    def pending(self) -> None:
+        self.state.pending()
+
+    def approve(self) -> None:
+        self.state.approve()
+
+    def reject(self) -> None:
+        self.state.reject()
+
+
+class OrderState(ABC):
+    '''State Abstract Class'''
+
+    def __init__(self, order: Order) -> None:
+        self.order = order
+
+    @abstractmethod
+    def pending(self) -> None: pass
+
+    @abstractmethod
+    def approve(self) -> None: pass
+
+    @abstractmethod
+    def reject(self) -> None: pass
+
+
+class PaymentPending(OrderState):
+    '''Concrete State Class'''
+
+    def pending(self) -> None:
+        print('The order is ALREADY in PENDING status')
+
+    def approve(self) -> None:
+        self.order.state = PaymentApproved(self.order)
+        print('State CHANGE | APPROVED')
+
+    def reject(self) -> None:
+        self.order.state = PaymentRejected(self.order)
+        print('State CHANGE | REJECTED')
+
+
+class PaymentApproved(OrderState):
+    '''Concrete State Class'''
+
+    def pending(self) -> None:
+        self.order.state = PaymentPending(self.order)
+        print('State CHANGE | PENDING')
+
+    def approve(self) -> None:
+        print('The order is ALREADY in APPROVED status')
+
+    def reject(self) -> None:
+        self.order.state = PaymentRejected(self.order)
+        print('State CHANGE | REJECTED')
+
+
+class PaymentRejected(OrderState):
+    '''Concrete State Class'''
+
+    def pending(self) -> None:
+        print('Impossible to change status to PENDING | REJECTED')
+
+    def approve(self) -> None:
+        print('Impossible to change status to APPROVED | REJECTED')
+
+    def reject(self) -> None:
+        print('The order is ALREADY in REJECTED status')
+
+
+if __name__ == '__main__':
+    # instances
+    order1 = Order()
+    order2 = Order()
+
+    # executions
+    order1.pending()
+    order1.approve()
+    order1.pending()
+    order1.reject()
+    order1.approve()
+    order1.pending()
+    order1.reject()
+
+    order2.pending()
+    order2.approve()
---
+++ behavioral/state/state_2.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class SoundSystem:
+    def __init__(self) -> None:
+        self.mode: PlayMode = ModeRadio(self)
+        self.playing = 0
+
+    def pressNext(self) -> None:
+        self.mode.pressNext()
+
+    def pressPrevious(self) -> None:
+        self.mode.pressPrevious()
+
+    def changeMode(self) -> None:
+        self.playing = 0
+        self.mode.changeMode()
+
+
+class PlayMode(ABC):
+    '''State Abstract Class'''
+
+    def __init__(self, device: SoundSystem) -> None:
+        self.device = device
+
+    @abstractmethod
+    def pressNext(self) -> None: pass
+
+    @abstractmethod
+    def pressPrevious(self) -> None: pass
+
+    @abstractmethod
+    def changeMode(self) -> None: pass
+
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__}'
+
+
+class ModeRadio(PlayMode):
+    '''State Concrete Class'''
+
+    def pressNext(self) -> None:
+        self.device.playing += 100
+        print(f'ADVANCING to the radio station | {self.device.playing}')
+
+    def pressPrevious(self) -> None:
+        self.device.playing -= 100 if self.device.playing > 0 else 0
+        print(f'GOING BACK to the radio station | {self.device.playing}')
+
+    def changeMode(self) -> None:
+        self.device.mode = ModeMp3(self.device)
+        print(f'CHANGE to mode | {self.device.mode}')
+
+
+class ModeMp3(PlayMode):
+    '''State Concrete Class'''
+
+    def pressNext(self) -> None:
+        self.device.playing += 1
+        print(f'ADVANCING to the mp3 file | {self.device.playing}')
+
+    def pressPrevious(self) -> None:
+        self.device.playing -= 1 if self.device.playing > 0 else 0
+        print(f'GOING BACK to the mp3 file | {self.device.playing}')
+
+    def changeMode(self) -> None:
+        self.device.mode = ModeRadio(self.device)
+        print(f'CHANGE to mode | {self.device.mode}')
+
+
+if __name__ == '__main__':
+    # instances
+    device = SoundSystem()
+
+    # executions
+    device.pressNext()
+    device.pressPrevious()
+    device.pressNext()
+    device.pressNext()
+    device.changeMode()
+    device.pressNext()
+    device.pressPrevious()
+    device.pressPrevious()
